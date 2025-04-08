f8f13b340bf1f0c39714892811b15fa13eb5d813
Kwantingwong <kwanting.wong@edu.sait.ca>
2025-04-08 10:51:52 -0600

    add all the validation

--- cpsy200/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/cpsy200.AssemblyInfo.cs
+++ cpsy200/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/cpsy200.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cpsy200")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bbfba95896ecf23483d10edc9d6f96d16c8323ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4c952c31cfb534e7e4d1b7864c62538389c01aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("cpsy200")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cpsy200")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
